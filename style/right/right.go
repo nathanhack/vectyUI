ef080cd03b3566c82d12731485dd50b384aabcf2
Nathan Hack <nathan.hack@gmail.com>
2019-12-25 17:41:18 -0500

    fixed copy paste bug

--- style/right/right.go
+++ style/right/right.go
@@ -11,11 +11,11 @@ const (
 )
 
 func (t Type) Apply(h *vecty.HTML) {
-	vecty.Style("top", string(t)).Apply(h)
+	vecty.Style("right", string(t)).Apply(h)
 }
 
 type Value Type
 
 func (v Value) Apply(h *vecty.HTML) {
-	vecty.Style("top", string(v)).Apply(h)
+	vecty.Style("right", string(v)).Apply(h)
 }
