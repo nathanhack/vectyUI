d010c9bae7089af5d5cd23314aa5b2391df28218
Nathan Hack <nathan.hack@gmail.com>
2019-12-25 16:14:37 -0500

    added output server address in output of examples

--- examples/simple/backgrounds/main.go
+++ examples/simple/backgrounds/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
-
 	if _, err := os.Stat("./static/wasm_exec.js"); os.IsNotExist(err) {
 		panic("File not found ./static/wasm_exec.js : find it in $GOROOT/misc/wasm/ note it must be from the same version of go used during compiling")
 	}
 
+	urlR, err := url.Parse("http://localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Server on: %v", urlR)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	err := http.ListenAndServe("localhost:8080", nil)
+	err = http.ListenAndServe(urlR.Host, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
